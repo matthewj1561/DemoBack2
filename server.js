c7982094c50385d283b2cf23551d4bf7a6a77c51
matthewj1561 <77714715+matthewj1561@users.noreply.github.com>
2022-04-20 09:23:09 -0600

    port change2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes/');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', routes);
 
